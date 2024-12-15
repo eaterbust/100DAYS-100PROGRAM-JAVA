67c982ba2d3cffd9d9b7bdb75be0193ace4ba92d
eater <44871495+eaterbust@users.noreply.github.com>
2024-12-16 00:09:14 +0700

    Add files via upload

---
+++ Day 2.java
@@ -0,0 +1,8 @@
+public class Day2 {
+    public static void main(String[]args) {
+        String nama="bibit";
+        int umur = 23;
+        System.out.println("Nama = " + nama);
+        System.out.println("Umur = " + umur);
+    }
+}
